3451d13ae1fe0b6920e666d941f6ee52714164a8
matias heredia <matiasheredia171292@gmail.com>
2024-06-09 18:41:10 -0300

    add endpoint tests

---
+++ app/tests/endpoints/location-endpoint.spec.js
@@ -0,0 +1,81 @@
+const mockery = require('mockery');
+const sinon = require('sinon');
+const should = require('should');
+const request = require('supertest');
+const express = require('express');
+
+describe('/v1/location', () => {
+    beforeEach(() => {
+        mockery.enable({
+            warnOnReplace: false,
+            warnOnUnregistered: false,
+            useCleanCache: true
+        });
+
+        mockery.registerMock('../config/config', {
+            port: 3000,
+            ipApiUrl: 'http://ipapitest',
+            weatherMap: {
+                url: 'http://weathermapurl',
+                apiKey: '123',
+                forecastDays: 5
+            }
+        });
+
+        mockery.registerMock('axios', (configuration) => {
+            let response = {};
+            switch(configuration.url) {
+                case 'http://ipapitest':
+                    response.data = {
+                        city: 'city',
+                        lat: 1,
+                        lon: -1,
+                        regionName: 'region',
+                        country: 'country'
+
+                    }
+                    break;
+                case 'http://weathermapurl/data/2.5/weather?lat=1&lon=-1&appid=123':
+                    response.data = true;
+            }
+            if(response.data === 'error') {
+                return Promise.reject('Error');
+            }
+            return Promise.resolve(response);
+        });
+
+        const router = require('../../routes/api-routes');
+        this.app = express();
+        this.app.use('/v1', router);
+
+    });
+
+    afterEach(() => {
+        mockery.deregisterAll();
+        mockery.resetCache();
+    });
+
+    it(('with city - status 200'), () => {
+        request(this.app)
+            .get('/v1/location')
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err;
+                res.body[0].city.should.equal('city');
+                res.body[0].lat.should.equal(1);
+                res.body[0].lon.should.equal(-1);
+                res.body[0].region.should.equal('region');
+                res.body[0].country.should.equal('country');
+            });
+    });
+
+    it(('with city - status 400'), () => {
+        request(this.app)
+            .get('/v1/current/testcityerror')
+            .expect(400)
+            .end(function(err, res) {
+                if (err) throw err;
+                res.body.message.should.equal('Error getting location');
+            });    
+    });
+})
